444ad8e385945f90fa4bb853dfe02b38d12ab7bf
Richard J Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-07-26 15:33:39 -0400

    Complete arrays file

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,50 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+ console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals.push("Gorilla");
+console.log(animals);
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals[2] = "Honey Badger";
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.push("Elephant");
+console.log(animals[4]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+  const foods = ["Strawberry", "Rice", "Chicken", "Steak"];
+  console.log(foods);
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.push("Broccoli");
+console.log(foods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
-
-
-// YOU DO: Write code to add 3 new foods to the array. 
+foods.pop();
+console.log(foods);
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
-
+foods.push("Broccoli", "Carrots", "Plantains");
+console.log(foods);
 // YOU DO: Remove the food that is in index position 0.
-
+foods.shift();
+console.log(foods);
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +86,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Twitter I can see doing this
+// 2: Facebook would be another
+// 3: Twitch for keeping your streams in a list
 
 
 //-------------------
@@ -96,7 +103,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,6 +113,21 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
+
+// the first conditional checks to see if the battery is less than 15% see if the client has a charger, if client doesnt have a charger, they cant call an uber
+
+  if (percentBatteryLeft < 15 & hasCharger === false) {
+    console.log("can't call an uber sorry");
+
+    // the second conditional is in the case they do have a charger, if the battery is less than 15% they can go call that Uber
+
+  } else if (percentBatteryLeft < 15 & hasCharger === true) {
+    console.log("go on ahead call that uber");
+
+    // if the phone is greater than 15% they can call an uber they dont need a charger
+  } else {
+  console.log("Call that uber!");
+};
 
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
